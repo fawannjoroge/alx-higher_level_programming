ca22e59b9400183eafbd71687f8c410ad4f063d0
fawannjoroge <fawannjoroge@gmail.com>
2024-10-02 07:51:17 +0300

    python import modules

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+if __name__ == '__main__':
+    largs = sys.argv
+    if (len(sys.argv)-1) == 0:
+        print('o arguments.')
+    elif(len(sys.argv)-1) == 1:
+        print("1 argument")
+    else:
+        print(f"{(len(sys.argv)-1):d} arguments:")
+    for i in range(1, len(largs)):
+        print(f"{i:d}: {largs[i]}")
---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+import hidden_4
+
+if __name__ == '__main__':
+    for i in dir(hidden_4):
+        if not i.startswith('__'):
+            print('{}'.format(i))
